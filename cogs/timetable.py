984687d5e8492ebfebc08d13c88e86a385244562
pyHAAA <toofastteemo@gmail.com>
2020-04-29 11:13:08 +1000

    fixes

--- cogs/timetable.py
+++ cogs/timetable.py
@@ -21,7 +21,7 @@ class Timetable(commands.Cog):
         title = student_info['title']
         student_id = str(student_info['id'])
         
-        if re.match(r'[a-zA-Z-]*\, [a-zA-Z-]*\|\d{1,}', [role.name for role in ctx.author.roles]):
+        if any(re.match(r'[a-zA-Z-]*\, [a-zA-Z-]*\|\d{1,}', role.name) for role in ctx.author.roles):
             embed = discord.Embed(title='Cannot assign role.',
                                   description=f'{ctx.author.mention} already identified as "{title}"',
                                   timestamp=datetime.datetime.now(tz=pytz.timezone('Australia/NSW')),    
